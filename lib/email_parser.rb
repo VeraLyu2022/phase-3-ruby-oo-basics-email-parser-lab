e100c3d9040e07145ff8669de71266a89f12abcd
Vera Lyu <465784942@qq.com>
2023-04-30 12:56:06 +1000

    with ChatGPT's help

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,15 @@
 # Build a class EmailAddressParser that accepts a string of unformatted 
+class EmailAddressParser
+    def initialize(addresses)
+        @addresses = addresses
+    end
+
+    def parse
+        @addresses.split(/[\s,]+/).uniq
+    end
+end
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+
